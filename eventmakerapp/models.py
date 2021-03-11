68f64d1f9418fe514e41a858a4d57452348c018e
erenoezveren <erenbabo2000@outlook.de>
2021-03-11 03:43:52 +0100

    Added relationships to the models

--- eventmakerapp/models.py
+++ eventmakerapp/models.py
@@ -1,39 +1,48 @@
 from django.db import models
 
 class Business(models.Model):
-    id = models.CharField(max_length=32, unique=True)
-    name = models.CharField(max_length=64)
+    name = models.CharField(max_length=64, null=False)
     #Password:
     email = models.EmailField(max_length=64)
     description = models.TextField()
     picture = models.ImageField()
+    website = models.URLField(max_length=64)
 
     def __str__(self):
         return self.name
 
 class Event(models.Model):
-    id = models.CharField(max_length=32, unique=True)
-    title = models.CharField(max_length=32)
-    location = models.CharField(max_length=128)
+    title = models.CharField(max_length=32, null=False)
+    location = models.CharField(max_length=128, null=False)
     picture = models.ImageField()
-    time = models.TimeField()
+    time = models.DateTimeField()
     price = models.IntegerField()
+    host = models.ForeignKey(
+        Business,
+        on_delete=models.CASCADE,
+    )
 
     def __str__(self):
         return self.title
 
 
 class User(models.Model):
-    id = models.CharField(max_length=32, unique=True)
-    username = models.CharField(max_length=64)
+    username = models.CharField(max_length=64, null=False)
     # Password:
     email = models.EmailField(max_length=64)
     bio = models.TextField()
     picture = models.ImageField()
+    likes = models.ManyToManyField(Event)
+    joins = models.ManyToManyField(Event)
+    comments = models.ManyToManyField(Event, through='Comment')
 
     def __str__(self):
         return self.name
 
 class Comment(models.Model):
-    id = models.CharField(max_length=64, unique=True)
-    data = models.TextField()
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
+    event = models.ForeignKey(Event, on_delete=models.CASCADE)
+    data = models.TextField(null=False)
+
+    def __str__(self):
+        return self.data
