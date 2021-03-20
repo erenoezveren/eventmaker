dc8de8258f6917e52968e0f61473f3c3959a9e57
erenoezveren <erenbabo2000@outlook.de>
2021-03-20 19:28:19 +0100

    Added map functionality

--- eventmakerapp/admin.py
+++ eventmakerapp/admin.py
@@ -1,6 +1,7 @@
 from django.contrib import admin
-from eventmakerapp.models import Event, User, Comment
+from eventmakerapp.models import Event, User, Comment, Place
 
 admin.site.register(Event)
 admin.site.register(User)
 admin.site.register(Comment)
+admin.site.register(Place)
