e9b6237eb64b76f4eb3d4cbcc4214e83193e7c24
erenoezveren <erenbabo2000@outlook.de>
2021-03-20 18:13:53 +0100

    Added location API

--- eventmakerapp/models.py
+++ eventmakerapp/models.py
@@ -1,4 +1,5 @@
 from django.db import models
+from location_field.models.plain import PlainLocationField
 
 class User(models.Model):
     is_business = models.BooleanField(default=False)
@@ -36,3 +37,7 @@ class Comment(models.Model):
 
     def __str__(self):
         return self.data
+
+class Place(models.Model):
+    city = models.CharField(max_length=255)
+    location = PlainLocationField(based_fields=['city'], zoom=7)
