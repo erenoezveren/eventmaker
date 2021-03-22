84dd4a3381a70848b2a9128a312bb6ca1dffe3d0
Alex Tonca <2437707t@student.gla.ac.uk>
2021-03-22 03:32:44 +0000

    some work on the homepage

--- eventmaker/settings.py
+++ eventmaker/settings.py
@@ -133,13 +133,9 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/2.2/howto/static-files/
 
 STATIC_URL = '/static/'
-STATICFILES_DIRS = [
-   os.path.join(BASE_DIR, "static"),
-]
+STATICFILES_DIRS = [STATIC_DIR, ]
 
 # Locations
-
-
 
 LOCATION_FIELD = {
     'map.provider': 'openstreetmap',
