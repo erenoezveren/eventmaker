ac62b6f74e8440ba3e17c75781b59660446d859c
Areeb Khan <2455329k@student.gla.ac.uk>
2021-04-05 15:08:27 +0100

    testing setup

--- eventmakerapp/tests.py
+++ eventmakerapp/tests.py
@@ -1,3 +1,14 @@
+import os
+import importlib
+from django.urls import reverse
 from django.test import TestCase
-
+from django.conf import settings
 # Create your tests here.
+FAILURE_HEADER = f"{os.linesep}{os.linesep}{os.linesep}================{os.linesep}Eventmaker Test Failure =({os.linesep}================{os.linesep}"
+FAILURE_FOOTER = f"{os.linesep}"
+
+class EventmakerUnitTests(TestCase):
+	def TestingSetUp(self):
+		self.views_module = importlib.import_module('eventmakerapp.views')
+		self.views_module_listing = dir(self.views_module)
+    
