91aadcba0520cf29be6adf458b592b3a7c23e9f3
Areeb Khan <2455329k@student.gla.ac.uk>
2021-04-06 14:22:41 +0100

    indentation errors fix

--- eventmakerapp/tests.py
+++ eventmakerapp/tests.py
@@ -11,6 +11,8 @@ class EventmakerUnitTests(TestCase):
 	def setUp(self):
 		self.views_module = importlib.import_module('eventmakerapp.views')
 		self.views_module_listing = dir(self.views_module)
+		self.functions_module = importlib.import_module('eventmakerapp.functions')
+		self.functions_module_listing = dir(self.functions_module)
 
 	def test_viewsExist(self):
 		#tests to determine whether all the necessary views are present in views.py
@@ -40,54 +42,55 @@ class EventmakerUnitTests(TestCase):
 		self.assertTrue(userprofile_exists, f"{failure_heading}Eventmaker userProfile view does not exist.")
 		self.assertTrue(addevent_exists, f"{failure_heading}Eventmaker addEvent view does not exist.")
 
-    def test_popular(self):
+	def test_aboutview(self):
+    	#test to check if the about page renders the correct html document
+  		pass
+	def test_user_profile_class(self):
+		
+        #Does the UserProfile class exist in eventmakerapp.models? If so, are all the required attributes present?
 
-    def test_userprofile_class(self):
-        """
-        Does the UserProfile class exist in eventmakerapp.models? If so, are all the required attributes present?
-        """
-        self.assertTrue('UserProfile' in dir(eventmakerapp.models))
+		self.assertTrue('UserProfile' in dir(eventmakerapp.models))
 
-        user_profile = eventmakerapp.models.UserProfile()
+		user_profile = eventmakerapp.models.UserProfile()
 
         # Now check that all the required attributes are present.
         # We do this by building up a UserProfile instance, and saving it.
-        expected_attributes = {
-            'first_name': 'Test',
-            'last_name': 'User',
-            'is_business': True,
-            'description': 'this is me',
-            'picture': 'party.jpg',
-            'user': create_user_object(),
-        }
+		expected_attributes = {
+			'first_name': 'Test',
+			'last_name': 'User',
+			'is_business': True,
+			'description': 'this is me',
+			'picture': 'party.jpg',
+			'user': create_user_object(),
+		}
 
-        expected_types = {
-            'first_name': models.fields.CharField,
-            'last_name': models.fields.CharField,
-            'is_business': models.fields.BooleanField,
-            'description': models.fields.TextField,
-            'picture': models.fields.files.ImageField,
-            'user': models.fields.related.OneToOneField,
-        }
+		expected_types = {
+			'first_name': models.fields.CharField,
+			'last_name': models.fields.CharField,
+			'is_business': models.fields.BooleanField,
+			'description': models.fields.TextField,
+			'picture': models.fields.files.ImageField,
+			'user': models.fields.related.OneToOneField,
+		}
+		
+		found_count = 0
 
-        found_count = 0
+		for attr in user_profile._meta.fields:
+			attr_name = attr.name
 
-        for attr in user_profile._meta.fields:
-            attr_name = attr.name
+			for expected_attr_name in expected_attributes.keys():
+				if expected_attr_name == attr_name:
+					found_count += 1
 
-            for expected_attr_name in expected_attributes.keys():
-                if expected_attr_name == attr_name:
-                    found_count += 1
+					self.assertEqual(type(attr), expected_types[attr_name],
+									f"{failure_heading}The type of attribute for '{attr_name}' was '{type(attr)}'; we expected '{expected_types[attr_name]}'. Please check your database. {failure_footing}")
+					setattr(user_profile, attr_name, expected_attributes[attr_name])
 
-                    self.assertEqual(type(attr), expected_types[attr_name],
-                                     f"{failure_heading}The type of attribute for '{attr_name}' was '{type(attr)}'; we expected '{expected_types[attr_name]}'. Please check your database. {failure_footing}")
-                    setattr(user_profile, attr_name, expected_attributes[attr_name])
+		self.assertEqual(found_count, len(expected_attributes.keys()),
+				f"{failure_heading}In the UserProfile model, we found {found_count} attributes, but were expecting {len(expected_attributes.keys())}. Please check your database.{failure_footing}")
+		user_profile.save()
 
-        self.assertEqual(found_count, len(expected_attributes.keys()),
-                         f"{failure_heading}In the UserProfile model, we found {found_count} attributes, but were expecting {len(expected_attributes.keys())}. Please check your database.{failure_footing}")
-        user_profile.save()
-
-    def test_user_form(self):
+	def test_user_form(self):
             """
             Tests whether UserForm is in the correct place, and whether the correct fields have been specified for it.
             """
@@ -114,12 +117,12 @@ class EventmakerUnitTests(TestCase):
                 self.assertEqual(expected_field, type(fields[expected_field_name]),
                                  f"{failure_heading}The field {expected_field_name} in UserForm was not of the correct type. Expected {expected_field}; got {type(fields[expected_field_name])}.{failure_footing}")
 
-    def test_user_profile_form(self):
+	def test_user_profile_form(self):
             """
             Tests whether UserProfileForm is in the correct place, and whether the correct fields have been specified for it.
             """
             self.assertTrue('UserProfileForm' in dir(forms),
-                            f"{failure_heading}Could not find UserProfileForm{failure_footing}")
+                            f"{failure_heading}Could not find UserProfileForm{failure_footing}")	
 
             user_profile_form = forms.UserProfileForm()
             self.assertEqual(type(user_profile_form.__dict__['instance']), eventmakerapp.models.UserProfile,
@@ -143,28 +146,28 @@ class EventmakerUnitTests(TestCase):
                 self.assertEqual(expected_field, type(fields[expected_field_name]),
                                  f"{failure_heading}The field {expected_field_name} in UserProfileForm was not of the correct type. Expected {expected_field}; got {type(fields[expected_field_name])}.{failure_footing}")
 
-    def test_login_function(self):
+	def test_login_function(self):
 
-        user_object = create_user_object()
-        response = self.client.post(reverse('eventmakerapp:login'), {'username': 'testuser', 'password': 'testabc123'})
+		user_object = create_user_object()
+		response = self.client.post(reverse('eventmakerapp:login'), {'username': 'testuser', 'password': 'testabc123'})
 
-        try:
-            self.assertEqual(user_object.id, int(self.client.session['_auth_user_id']),
-                f"{failure_heading}Attempted to log a user in with an ID of {user_object.id}, but instead logged in with a user of ID {self.client.session['_auth_user_id']}.{failure_footing}")
-        except KeyError:
-            self.assertTrue(False,
-                f"{failure_heading}When attempting to log in with your login() view, it didn't seem to log the user in. Please check your login() view implementation, and try again.{failure_footing}")
+		try:
+			self.assertEqual(user_object.id, int(self.client.session['_auth_user_id']),
+				f"{failure_heading}Attempted to log a user in with an ID of {user_object.id}, but instead logged in with a user of ID {self.client.session['_auth_user_id']}.{failure_footing}")
+		except KeyError:
+			self.assertTrue(False,
+					f"{failure_heading}When attempting to log in with your login() view, it didn't seem to log the user in. Please check your login() view implementation, and try again.{failure_footing}")
 
-        self.assertEqual(response.status_code, 302,
-                         f"{failure_heading}When attempting to log in with your login() view, it didn't seem to log the user in. Please check your login() view implementation, and try again.{failure_footing}")
-        self.assertEqual(response.url, reverse('eventmakerapp:index'),
-                         f"{failure_heading}We were not redirected to the Eventmaker homepage after logging in. Please check your login() view implementation, and try again.{failure_footing}")
+		self.assertEqual(response.status_code, 302,
+						f"{failure_heading}When attempting to log in with your login() view, it didn't seem to log the user in. Please check your login() view implementation, and try again.{failure_footing}")
+		self.assertEqual(response.url, reverse('eventmakerapp:index'),
+				f"{failure_heading}We were not redirected to the Eventmaker homepage after logging in. Please check your login() view implementation, and try again.{failure_footing}")
 
 
-    def test_Index(self):
+	def test_Index(self):
         # test to check if the index_helper function is callable
-        self.assertTrue(callable(self.functions_module.index_helper),
-                        f"{failure_heading}index helper function is not defined correctly")
+		self.assertTrue(callable(self.functions_module.index_helper),
+				f"{failure_heading}index helper function is not defined correctly")
 
 
 def create_user_object():
@@ -189,4 +192,4 @@ def create_events():
 
     ##still working here
     )
-    )
+    
